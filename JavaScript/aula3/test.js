068aceb14f5d942d13a376c99dd501a84fa19d02
Zigetsu <rapha.soares4.rsf@gmail.com>
2024-04-23 13:12:18 -0300

    treinos com let

---
+++ JavaScript/aula3/test.js
@@ -0,0 +1,42 @@
+
+let nome = 'João';
+let nome2 = 'Maria';
+let nome3 = 'Eduardo'
+
+
+console.log(`${nome} nasceu em 2011`);
+console.log(`Em 2024 ${nome} conheceu ${nome2}`);
+console.log(`${nome} casou-se com ${nome2} em 2031`);
+console.log(`${nome2} teve um filho com ${nome} em 2033`);
+console.log(`O filho de ${nome} se chama ${nome3}`);
+
+
+let teste; //Declarou a variavel
+teste = 'skibidi' //inicializando a variável
+console.log(teste)
+
+//Não podemos criar variaveis com palavras reservadas
+/*
+
+let if = 'João';
+let console;
+
+*/
+
+//variaveis precisam ter nomes significativos
+
+let nomeEmpresa = 'Empresa X' //utilizamos camel case
+console.log(nomeEmpresa)
+
+//não pode começar com um numero
+
+let no1me;
+
+//não pode conter espaços ou traços
+
+//case-sensitive
+
+let nomeCompleto = 'Raphael';
+let nomecompleto = 'soares';
+
+console.log(nomeCompleto, nomecompleto);
