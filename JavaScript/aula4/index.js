a2bd8accf029bdcfc90eeab107c7aa451df08c20
Zigetsu <rapha.soares4.rsf@gmail.com>
2024-04-24 13:05:47 -0300

    treinos com const

---
+++ JavaScript/aula4/index.js
@@ -0,0 +1,8 @@
+const primeiroNumero = 7;
+const segundoNUmero = 9;
+
+const resultado = primeiroNumero * segundoNUmero;
+const resultadoDuplicado = resultado * 2;
+
+
+console.log(resultadoDuplicado);
