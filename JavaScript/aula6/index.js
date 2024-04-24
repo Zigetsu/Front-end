eef7f4341b52231eda7dd3456f378400657aa07b
Zigetsu <rapha.soares4.rsf@gmail.com>
2024-04-24 14:53:41 -0300

    dados primitivos

---
+++ JavaScript/aula6/index.js
@@ -0,0 +1,8 @@
+const nome = 'Raphael'; //string
+const nome1 = "Raphael"; //string
+const nome2 = `Raphael`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let saborSorvete; //undefined --> não aponta para local nenhum na memória
+let caldaSorvete = null; //Nulo --> não aponta para local nenhum na memória
+const aprovado = false; //Boolean = true ou false
