61a5b8ac83447b3266e8d40f0ebec741c15d6bc9
Zigetsu <rapha.soares4.rsf@gmail.com>
2024-04-25 14:34:47 -0300

    dados primitivos

---
+++ JavaScript/aula7/index.js
@@ -0,0 +1,27 @@
+/**
+ *  Aritméticos
+ *  + adição - concatenação
+ *  * multiplicação
+ *  divisão
+ *  ** potenciação
+ *  % resto da divisão
+ */
+
+/*
+ Ordem de precedencia
+
+  ()
+  **
+  * / %
+  + -
+*/
+
+/*
+  Incremento = ++
+  Decremento = --
+*/
+
+const num1 = 5;
+const num2 = Number('9.3');
+
+console.log(num1 + num2);
