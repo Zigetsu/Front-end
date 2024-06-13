f2efad83a040da2e80693012570240f3d9a2c321
Zigetsu <rapha.soares4.rsf@gmail.com>
2024-06-13 19:00:02 -0300

    string

--- JavaScript/aula10/index.js
+++ JavaScript/aula10/index.js
@@ -2,11 +2,15 @@
 let umaString = 'um texto';
 
 //formas de concatenação de um texto
-console.log(umaString.concat(' em um lindo dia.'));
-console.log(umaString + (' em um lindo dia.'))
+//console.log(umaString.concat(' em um lindo dia.'));
+//console.log(umaString + (' em um lindo dia.'))
 console.log(`${umaString} em um lindo dia.`)
 
-console.log(umaString[4]);
+console.log(umaString.length);
+//                 012345678910
+let outraString = "outro texto";
+
+console.log(outraString.indexOf('o')); 
 
 
 
